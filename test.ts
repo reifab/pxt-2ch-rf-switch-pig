9d36ac0a169234678ffad74d71c32e68c69c3f53
reifab <72970313+reifab@users.noreply.github.com>
2021-09-15 13:50:31 +0200

    basic functionality and interface implemented

--- test.ts
+++ test.ts
@@ -1,1 +1,12 @@
 // Hier kann man Tests durchführen; diese Datei wird nicht kompiliert, wenn dieses Paket als Erweiterung verwendet wird.
+switchPig.selectRfGroupID(MyEnumGroupIDs.Nif)
+basic.forever(function () {
+    switchPig.switchPlug(MyEnumPlugLabel.K1, MyEnumState.ON)
+    basic.pause(1000)
+    switchPig.switchPlug(MyEnumPlugLabel.K1, MyEnumState.OFF)
+    basic.pause(1000)
+    switchPig.switchPlug(MyEnumPlugLabel.K2, MyEnumState.ON)
+    basic.pause(1000)
+    switchPig.switchPlug(MyEnumPlugLabel.K2, MyEnumState.OFF)
+    basic.pause(1000)
+})
